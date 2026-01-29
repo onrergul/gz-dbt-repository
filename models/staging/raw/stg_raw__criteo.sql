0597f0890abbbfc53517d16b0c07b2231106737a
Mehmet Onur Ergul <onrergul@gmail.com>
2026-01-29 18:27:26 +0000

    Add paid ads staging models + int_campaigns + update schema docs

---
+++ models/staging/raw/stg_raw__criteo.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'criteo') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        camPGN_name as campaign_name,
+        SAFE_CAST(ads_cost AS FLOAT64) AS ads_cost,
+        impression as impressions,
+        click as clicks
+
+    from source
+
+)
+
+select * from renamed
